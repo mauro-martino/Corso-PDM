bfd34809ba85f040a9a78d563f5ade3355aa8049
Mauro Martino <iwgnma@gmail.com>
2011-12-19 13:39:21 +0100

    inserimento ese DueActivity

---
+++ DueActivity/src/pdm/ese/Second.java
@@ -0,0 +1,13 @@
+package pdm.ese;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Second extends Activity{
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+
+}
